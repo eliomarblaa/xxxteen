57cad6299fa8a16474cab976dc63ce050240c419
eliomarbla <terter78@live.com>
2015-10-03 16:35:59 -0300

    Program.cs
    Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+
+using EloBuddy.SDK.Events;
+
+namespace DravenPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += eventArgs => Draven.OnLoad();
+        }
+    }
+}
